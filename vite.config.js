42443e4f3c08771025c8d302f1f6d6b1be99ba82
Klu2300032633 <2300032633@kluniversity.in>
2025-10-18 10:47:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // The crucial line to fix Tomcat deployment:
+  base: '/hospital-ui/', 
+  
   plugins: [react()],
+  // ...
 })
